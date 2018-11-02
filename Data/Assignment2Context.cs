b922b881c174c3145d115110d5bce6bbfc489080
Bruno Lopes <bruno87abud@gmail.com>
2018-11-02 16:30:17 -0400

    My first commit

---
+++ Data/Assignment2Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using assignment02.Models;
+
+namespace Assignment2.Models
+{
+    public class Assignment2Context : DbContext
+    {
+        public Assignment2Context (DbContextOptions<Assignment2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<assignment02.Models.Member> Member { get; set; }
+    }
+}
